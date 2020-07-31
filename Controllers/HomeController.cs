a0579a0848c9a1e926d7c03beca0c567d94dbf60
Bhuwan Shahi <shahibhuwan5555@gmail.com>
2020-07-31 07:14:28 +0545

    My portfolio using asp.net mvc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using MyPortfolioManagement.Models;
+
+namespace MyPortfolioManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendEmail(Email e)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress(e.email, e.Name);
+                    var receiverEmail = new MailAddress("shahibhuwan5555@gmail.com", "Receiver");
+                    var password = e.password;
+                   
+                    var body = e.Message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(e.email, e.password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = "PorFolio alert!",
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View(e);
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View(e);
+
+
+
+
+        }
+    }
+}
---
+++ Models/Email.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolioManagement.Models
+{
+    public class Email
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        public string Number { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty and must email followed by ''@''")]
+        public string email { get; set; }
+        
+        [Required(ErrorMessage = "Field can't be empty")]
+        public string Message { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        public string password { get; set; }
+    }
+}
